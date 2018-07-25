9fece84e65f4c6bba5e4aeb271bc2cf609fea836
Nayab Fathima <zara586243@gmail.com>
2018-07-25 16:24:06 +0530

    added pattern

--- LED.py
+++ LED.py
@@ -1,11 +1,16 @@
 from flask import Flask,request,render_template,jsonify
 from gpiozero import LED
-
+import time
 #from led_on import LED_ON
 #from led_off import LED_OFF
 
 app = Flask(__name__)
-led=LED(18)
+led1=LED(14)
+led2=LED(15)
+led3=LED(18)
+led4=LED(2)
+led5=LED(3)
+led6=LED(4)
 
 @app.route('/',methods=['GET'])
 def index():
@@ -15,18 +20,48 @@ def index():
 def pin_on():
     if request.method == 'POST':
         body=request.get_json()
-        led.on()
+        for i in range (0,5)
+        led1.on()
+        time.sleep(1)
+        led2.on()
+        time.sleep(1)
+        led1.off()
+        led2.off()
+
+        led3.on()
+        time.sleep(1)
+        led4.on()
+        time.sleep(1)
+        led3.off()
+        led4.off()
+
+        led5.on()
+        time.sleep(1)
+        led6.on()
+        time.sleep(1)
+        led5.off()
+        led6.off()
+
+        led1.on()
+        led3.on()
+        led5.on()
+        time.sleep(1)
+        led1.off()
+        led3.off()
+        led5.off()
+
+        led2.on()
+        led4.on()
+        led6.on()
+        time.sleep(1)
+        led2.off()
+        led4.off()
+        led6.off()
+
         return jsonify({'status' : 'LED_ON(body[])'})
     else:
         return jsonify({'status: unavailable'})
-@app.route('/pin_off', methods=['GET', 'POST'])
-def pin_off():
-    if request.method == 'POST':
-        body=request.get_json()
-        led.off()
-        return jsonify({'status' : 'LED_OFF(body[])'})
-    else:
-        return jsonify({'status: unavailable'})
+
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0', debug=True)
